027cbd87b171ceeeffab540870647f53f4ea24aa
Abdelrahman Hany <abdelrahman_hany_cw@lineaje.com>
2026-03-05 12:09:18 +0200

    trigger test

--- src/main/java/com/project/shopybackend/CartController.java
+++ src/main/java/com/project/shopybackend/CartController.java
@@ -10,35 +10,45 @@ public class CartController {
     private CartService cartService;
 
     @PostMapping
-    public String addCart(@RequestBody Cart cart){
+    public String addCart(@RequestBody Cart cart) {
         cartService.addCart(cart);
         return "Cart added successfully!";
     }
 
     @GetMapping("/{userId}")
-    public Cart getCartByUserId(@PathVariable String userId){return cartService.getCartByUserId(userId);}
+    public Cart getCartByUserId(@PathVariable String userId) {
+        return cartService.getCartByUserId(userId);
+    }
 
     @PutMapping("/{userId}")
-    public void updateCart(@PathVariable String userId, @RequestBody Cart cart){cartService.editCart(userId,cart);}
+    public void updateCart(@PathVariable String userId, @RequestBody Cart cart) {
+        cartService.editCart(userId, cart);
+    }
 
     @PutMapping("/{userId}/addproduct")
-    public void addProductToCart(@PathVariable String userId, @RequestBody OrderProduct orderProduct){
+    public void addProductToCart(@PathVariable String userId, @RequestBody OrderProduct orderProduct) {
         cartService.addProductToCart(userId, orderProduct.getProductId(), orderProduct.getQuantity());
     }
 
     @PutMapping("/{userId}/checkout")
-    public void checkOut(@PathVariable String userId){
+    public void checkOut(@PathVariable String userId) {
         cartService.checkOut(userId);
     }
 
     @PutMapping("/{userId}/editproductquantity")
-    public void editProductQuantity(@PathVariable String userId, @RequestBody OrderProduct orderProduct){
+    public void editProductQuantity(@PathVariable String userId, @RequestBody OrderProduct orderProduct) {
         cartService.editProductQuantity(userId, orderProduct.getProductId(), orderProduct.getQuantity());
     }
 
     @PutMapping("/{userId}/clearcart")
-    public void clearCart(@PathVariable String userId){cartService.clearCart(userId);}
+    public void clearCart(@PathVariable String userId) {
+        cartService.clearCart(userId);
+    }
 
     @DeleteMapping("/{userId}")
-    public void deleteCart(@PathVariable String userId){cartService.deleteCart(userId);}
+    public void deleteCart(@PathVariable String userId) {
+        cartService.deleteCart(userId);
+    }
 }
+
+// new comment to test trigger
